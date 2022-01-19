e3c83921bf422c704bd60c77132917f42da080a1
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-19 15:00:15 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.etlap {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.sql;
+
+
+    opens com.example.etlap to javafx.fxml;
+    exports com.example.etlap;
+}
