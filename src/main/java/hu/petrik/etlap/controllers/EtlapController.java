fe3fbe8ba20717aad9c9eae8a82e74284890518a
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-27 11:07:35 +0100

    refactoring

---
+++ src/main/java/hu/petrik/etlap/controllers/EtlapController.java
@@ -0,0 +1,8 @@
+package hu.petrik.etlap.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EtlapController {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.example.etlap {
 
     opens hu.petrik.etlap to javafx.fxml;
     exports hu.petrik.etlap;
+    exports hu.petrik.etlap.controllers;
+    opens hu.petrik.etlap.controllers to javafx.fxml;
 }
