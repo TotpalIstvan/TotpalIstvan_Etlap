21cf6e94a722d7ae99a71ea3f2796077bf43b02a
TotpalIstvan <totpal.istvan@petrik.hu>
2022-02-06 14:27:54 +0100

    view kész controller elkezdve

--- src/main/java/hu/petrik/etlap/controllers/EtlapController.java
+++ src/main/java/hu/petrik/etlap/controllers/EtlapController.java
@@ -1,7 +1,23 @@
 package hu.petrik.etlap.controllers;
 
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 
 public class EtlapController {
 
+    public void etelHozzaadasButton(ActionEvent actionEvent) {
+    }
+
+    public void etelTorlesButton(ActionEvent actionEvent) {
+    }
+
+    public void szazalekkalEmeles(ActionEvent actionEvent) {
+    }
+
+    public void osszegelEmeles(ActionEvent actionEvent) {
+    }
+
+    public void etelAdat (MouseEvent mouseEvent) {
+    }
 }
