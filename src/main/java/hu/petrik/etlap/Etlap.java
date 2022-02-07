a99015288b8d55ff77df33a9dd62c8ac45771363
TotpalIstvan <totpal.istvan@petrik.hu>
2022-02-07 15:48:29 +0100

    étlap view, controller és db kész

---
+++ src/main/java/hu/petrik/etlap/Etlap.java
@@ -0,0 +1,53 @@
+package hu.petrik.etlap;
+
+public class Etlap {
+    private  int id;
+    private int cost;
+    private String name;
+    private String description;
+    private String category;
+
+    public Etlap(int id, int cost, String name, String description, String category) {
+        this.id = id;
+        this.cost = cost;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
--- src/main/java/hu/petrik/etlap/controllers/EtlapController.java
+++ src/main/java/hu/petrik/etlap/controllers/EtlapController.java
@@ -1,10 +1,49 @@
 package hu.petrik.etlap.controllers;
 
 
+import hu.petrik.etlap.Etlap;
+import hu.petrik.etlap.db.Etlapdb;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class EtlapController {
+
+    @FXML
+    private TableView<Etlap> dbTabla;
+    @FXML
+    private TableColumn<Etlap, String> nevOszlop;
+    @FXML
+    private TableColumn<Etlap, String> kategoriaOszlop;
+    @FXML
+    private TableColumn<Etlap, Integer> arOszlop;
+    @FXML
+    private TextArea leiras;
+
+    private Etlapdb db;
+
+    public EtlapController() {
+        nevOszlop.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        kategoriaOszlop.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
+        arOszlop.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        try {
+            db = new Etlapdb();
+            List<Etlap> menusor = db.getEtlap();
+
+            for (Etlap elem: menusor) {
+                dbTabla.getItems().add(elem);
+            }
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 
     public void etelHozzaadasButton(ActionEvent actionEvent) {
     }
@@ -19,5 +58,7 @@ public class EtlapController {
     }
 
     public void etelAdat (MouseEvent mouseEvent) {
+        Etlap ennivalo = dbTabla.getSelectionModel().getSelectedItem();
+        leiras.setText(ennivalo.getDescription());
     }
 }
